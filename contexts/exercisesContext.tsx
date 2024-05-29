08a2ee6d61ec983ef455b94801094eab15a61881
Takuma Yoshimi <ysm0706glee@gmail.com>
2024-05-29 20:44:30 +0200

    add exercises and menus contexts

---
+++ contexts/exercisesContext.tsx
@@ -0,0 +1,118 @@
+import { createContext, useContext, useState } from "react";
+import { supabase } from "@/libs/supabase";
+import { Tables } from "@/types/supabase";
+
+type ExercisesContext = {
+  exercises: Tables<"exercises">[];
+};
+
+const ExercisesContext = createContext<ExercisesContext>({
+  exercises: [],
+});
+
+type ExercisesDispatch = {
+  getMenuExercises: (menuId: Tables<"menus">["id"]) => Promise<
+    {
+      exercises: Tables<"exercises"> | null;
+    }[]
+  >;
+  deleteMenusExercises: (menuId: Tables<"menus">["id"]) => Promise<void>;
+  postExercises: (
+    name: Tables<"exercises">["name"],
+    memo: Tables<"exercises">["memo"]
+  ) => Promise<Tables<"exercises">[]>;
+  postMenusExercises: (
+    menuId: Tables<"menus">["id"],
+    exerciseId: Tables<"exercises">["id"]
+  ) => Promise<void>;
+};
+
+const ExercisesDispatchContext = createContext<ExercisesDispatch>({
+  getMenuExercises: () => {
+    throw Error("no default value");
+  },
+  deleteMenusExercises: () => {
+    throw Error("no default value");
+  },
+  postExercises: () => {
+    throw Error("no default value");
+  },
+  postMenusExercises: () => {
+    throw Error("no default value");
+  },
+});
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export function ExercisesProvider({ children }: Props) {
+  const [exercises, setExercises] = useState<Tables<"exercises">[]>([]);
+
+  async function getMenuExercises(menuId: Tables<"menus">["id"]) {
+    const { data, error } = await supabase
+      .from("menus_exercises")
+      .select("exercises (*)")
+      .eq("menu_id", menuId);
+    if (error) throw error;
+    const transformedExercises = data
+      .map((x) => x.exercises)
+      // TODO: delete type after updating typescript
+      .filter((exercise): exercise is Tables<"exercises"> => exercise !== null);
+    setExercises(transformedExercises);
+    return data;
+  }
+
+  async function deleteMenusExercises(menuId: Tables<"menus">["id"]) {
+    const { error } = await supabase
+      .from("menus_exercises")
+      .delete()
+      .eq("menu_id", menuId);
+    if (error) throw error;
+  }
+
+  async function postExercises(
+    name: Tables<"exercises">["name"],
+    memo: Tables<"exercises">["memo"]
+  ) {
+    const { data, error } = await supabase
+      .from("exercises")
+      .insert({ name, memo })
+      .select();
+    if (error) throw error;
+    return data;
+  }
+
+  async function postMenusExercises(
+    menuId: Tables<"menus">["id"],
+    exerciseId: Tables<"exercises">["id"]
+  ) {
+    const { error } = await supabase
+      .from("menus_exercises")
+      .insert({ menu_id: menuId, exercise_id: exerciseId });
+    if (error) throw error;
+  }
+
+  return (
+    <ExercisesContext.Provider value={{ exercises }}>
+      <ExercisesDispatchContext.Provider
+        value={{
+          getMenuExercises,
+          deleteMenusExercises,
+          postExercises,
+          postMenusExercises,
+        }}
+      >
+        {children}
+      </ExercisesDispatchContext.Provider>
+    </ExercisesContext.Provider>
+  );
+}
+
+export function useExercises() {
+  return useContext(ExercisesContext);
+}
+
+export function useExercisesDispatch() {
+  return useContext(ExercisesDispatchContext);
+}
---
+++ contexts/menusContext.tsx
@@ -0,0 +1,111 @@
+import React, { createContext, useContext, useState } from "react";
+import { supabase } from "@/libs/supabase";
+import { Tables } from "@/types/supabase";
+
+type MenusContext = {
+  menus: Tables<"menus">[];
+  menu: Tables<"menus"> | null;
+};
+
+const MenusContext = createContext<MenusContext>({
+  menus: [],
+  menu: null,
+});
+
+type MenusDispatch = {
+  getMenus: () => Promise<Tables<"menus">[]>;
+  getMenu: (id: Tables<"menus">["id"]) => Promise<Tables<"menus">>;
+  deleteMenus: (menuId: Tables<"menus">["id"]) => Promise<void>;
+  postMenus: (
+    name: Tables<"menus">["name"],
+    memo: Tables<"menus">["memo"]
+  ) => Promise<Tables<"menus">[]>;
+};
+
+const MenusDispatchContext = createContext<MenusDispatch>({
+  getMenus: () => {
+    throw Error("no default value");
+  },
+  getMenu: () => {
+    throw Error("no default value");
+  },
+  deleteMenus: () => {
+    throw Error("no default value");
+  },
+  postMenus: () => {
+    throw Error("no default value");
+  },
+});
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export function MenusProvider({ children }: Props) {
+  const [menus, setMenus] = useState<Tables<"menus">[]>([]);
+  const [menu, setMenu] = useState<Tables<"menus"> | null>(null);
+
+  async function getMenus() {
+    const { data, error } = await supabase.from("menus").select("*");
+    if (error) throw error;
+    setMenus(data);
+    return data;
+  }
+
+  async function getMenu(id: Tables<"menus">["id"]) {
+    const { data, error } = await supabase
+      .from("menus")
+      .select("*")
+      .eq("id", id)
+      .single();
+    if (error) throw error;
+    setMenu(data);
+    return data;
+  }
+
+  async function deleteMenus(menuId: Tables<"menus">["id"]) {
+    const { error } = await supabase.rpc("delete_menu", {
+      menuid: menuId,
+    });
+    if (error) throw error;
+  }
+
+  async function postMenus(
+    name: Tables<"menus">["name"],
+    memo: Tables<"menus">["memo"]
+  ) {
+    const userId = (await supabase.auth.getUser()).data.user?.id;
+    if (!userId) {
+      throw new Error("User is not authenticated");
+    }
+    const { data, error } = await supabase
+      .from("menus")
+      .insert([{ name, memo, user_id: userId }])
+      .select();
+    if (error) throw error;
+    return data;
+  }
+
+  return (
+    <MenusContext.Provider value={{ menus, menu }}>
+      <MenusDispatchContext.Provider
+        value={{
+          getMenus,
+          getMenu,
+          deleteMenus,
+          postMenus,
+        }}
+      >
+        {children}
+      </MenusDispatchContext.Provider>
+    </MenusContext.Provider>
+  );
+}
+
+export function useMenus() {
+  return useContext(MenusContext);
+}
+
+export function useMenusDispatch() {
+  return useContext(MenusDispatchContext);
+}
